7ca463187af668ba83ad05b32c7584a73da42572
Adil Myktybek <adilmyktybek@Adils-MacBook-Pro.local>
2023-02-24 11:46:32 +0600

    [AM-Final] - Init project

---
+++ src/test/java/kz/axelrod/finalproject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.axelrod.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
